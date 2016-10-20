fix problem with algorithm using normalization

content.py
@@ -4,33 +4,31 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import numpy
 
-modelLoaded = False
-r = []
-SIMKEY = 'p:smlr:%s'
-courses = []
+modelLoaded = False		# stores whether or not the machine learning model is ready to use
+r = []					# just an array to store the results
+SIMKEY = 'p:smlr:%s'	# stores the id; not really important
+courses = []			# stores the list of possible courses
 
 def train(data_source):
-	global courses
-	global modelLoaded
-	start = time.time()
-	ds = pd.read_csv(data_source)
+	global courses			# makes sure these are global variables
+	global modelLoaded		# ''''
+	start = time.time()		# times how long it takes to train the model
+	ds = pd.read_csv(data_source)	# gets the information from the csv (in this case /static/storage/classes.csv)
 	print("Training data ingested in %s seconds." % (time.time() - start))
 	start = time.time()
-	courses = ds["course"].values.tolist()
-	_train(ds)
+	courses = ds["course"].values.tolist()	# turns the csv data into an array
+	_train(ds)								# trains the model
 	print("Engine trained in %s seconds." % (time.time() - start))
-	modelLoaded = True
+	modelLoaded = True			# ready to go!
 
 def _train(ds):
-	tf = TfidfVectorizer(analyzer='word',
+	tf = TfidfVectorizer(analyzer='word',			# sets up the matrix (math stuff)
                          ngram_range=(1, 3),
                          min_df=0,
                          stop_words='english')
 	tfidf_matrix = tf.fit_transform(ds['description'])
-
 	cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-	for idx, row in ds.iterrows():
+	for idx, row in ds.iterrows():		# goes through all the courses
 		similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
 		similar_items = [(cosine_similarities[idx][i], ds['id'][i]) for i in similar_indices]
 
@@ -38,7 +36,7 @@ def _train(ds):
 		# This 'sum' is turns a list of tuples into a single tuple:
 		# [(1,2), (3,4)] -> (1,2,3,4)
 		flattened = sum(similar_items[1:], ())
-		r.append([SIMKEY % row['id'], *flattened])
+		r.append([SIMKEY % row['id'], *flattened])		# appends the list of similar courses to a particular course
 
 def predict(course_id, num=10):
 	global courses