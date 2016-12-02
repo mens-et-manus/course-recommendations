f9c017b484aa865f941191b8fb5cef9ae3f10b70
mkaminsky11 <mkaminsky11@gmail.com>
2016-12-02 13:39:13 -0500

    dump

--- startLinux.sh
+++
@@ -1,8 +0,0 @@
-# start mongo
-sudo mongod --fork --logpath data/log/mongod.log --dbpath data/db
-# start apache
-
-# TODO
-
-# start server
-uwsgi --ini courserecs.ini --plugin python3 --chmod-socket=666
